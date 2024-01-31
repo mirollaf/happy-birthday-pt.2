a03e30e7697096cc11992b4df0566c1f1b1219c9
mirollaf <mirollaf07@gmail.com>
2024-01-31 13:46:03 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function showEvent2() {
+    document.getElementById("title").innerText = "title1"
+    document.getElementById("info").innerText = "test2"
+    document.getElementById("img1").src = ""
+}
+function showEvent3() {
+    document.getElementById("title").innerText = "title2"
+
+    document.getElementById("info").innerText = "test3"
+    document.getElementById("img1").src = ""
+}
+function showEvent4() {
+    document.getElementById("title").innerText = "title3"
+
+    document.getElementById("info").innerText = "test4"
+    document.getElementById("img1").src = ""
+}
+function showEvent5() {
+    document.getElementById("title").innerText = "title4"
+
+    document.getElementById("info").innerText = "test5"
+    document.getElementById("img1").src = ""
+}
